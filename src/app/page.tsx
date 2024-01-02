4f25beb298255539902e421adc5b4e0fa82a0e3b
Kspsss <cjk123cjk@gmail.com>
2024-01-02 23:10:16 +0800

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="flex  flex-col items-center justify-between bg-blue-300  p-24 h-full">
+
+      <div className="flex flex-col  h-50">
+        <div>
+          <h1>Hi, I am Jack, Welcome to my website</h1>
+        </div>
+      </div>
+      <div>
+        My skill
+
+      </div>
+
+
+    </main>
+  )
+}
